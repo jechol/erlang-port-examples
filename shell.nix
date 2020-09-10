31375880aef0ac2e1210dc163cee9b47d461b0e3
Jechol Lee <mr.jechol@gmail.com>
2020-09-10 23:15:50 +0900

    lorrify

---
+++ shell.nix
@@ -0,0 +1,11 @@
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { };
+  jechol-nur = import sources.jechol-nur { };
+  inherit (pkgs.lib) optional optionals;
+in pkgs.mkShell {
+  buildInputs = [
+    jechol-nur.beam.main.erlangs.erlang_22_0
+    jechol-nur.beam.main.packages.erlang_22_0.elixirs.elixir_1_9_0
+  ];
+}
